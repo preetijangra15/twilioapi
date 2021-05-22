f261df58e839da3b16aa2e6acbff3d3c1cc521e3
preetijangra15 <68685553+preetijangra15@users.noreply.github.com>
2021-05-22 21:33:50 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,54 +1,27 @@
-import os
-
-from dotenv import load_dotenv
-from flask import (
-    Flask,
-    flash, 
-    render_template, 
-    redirect,
-    request,
-    url_for,
-)
-
-load_dotenv()
-app = Flask(__name__)
-app.secret_key = "ssssh don't tell anyone"
-
+from flask import Flask, render_template, request
 from twilio.rest import Client
+app = Flask(__name__)
 
 client = Client(
     "AC9947a5728fd4dfc0eeb2065dd2d200bf", 
-    "fa6ab9b411e19da0f7ac4369430860b6"
+    "enter auth token"
 )
 
-def get_sent_messages():
-    # TODO: Make this return a collection of messages that were sent from the number
-    messages = []
-    return messages
-
-def send_message(to, body):
-    msg = client.messages.create(
-     to =to,
-     from_ = "+16692018325",
-     body = body,
- )
-    
-
-@app.route("/", methods=["GET"])
+@app.route('/')
 def index():
-    messages = get_sent_messages()
-    return render_template("index.html", messages=messages)
+    return render_template('index.html')
 
-@app.route("/add-compliment", methods=["POST"])
-def add_compliment():
-    sender = request.values.get('sender', 'Someone')
-    receiver = request.values.get('receiver', 'Someone')
-    compliment = request.values.get('compliment', 'wonderful')
-    to = request.values.get('to')
-    body = f'{sender} says: {receiver} is {compliment}. See more compliments at {request.url_root}'
-    send_message(to, body)
-    flash('Your message was successfully sent')
-    return redirect(url_for('index'))
 
-if __name__ == '__main__':
-    app.run()
+
+@app.route('/result',methods = ['POST', 'GET'])
+def result():
+   if request.method == 'POST':
+      first_name = request.form.get("Name")
+      result = request.form
+      print(first_name)
+      client.messages.create(
+          to ="+919050585522",
+          from_ = "+16692018325",
+          body= first_name,
+      )
+      return render_template("index.html",result = result)
